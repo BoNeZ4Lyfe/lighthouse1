e956985c3a0aeaedb9bee9abcc69613d45990235
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2021-11-22 22:26:32 -0500

    head.js exercise

---
+++ lighthouse1/head.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+function head(index) {
+  return index[0];
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]));
